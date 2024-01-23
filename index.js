2444d8d3dec134c9907799d6918fe2205a13762a
Raktim Shrestha <raktim@rumsan.com>
2024-01-23 18:58:10 +0545

    learned about variables, comments and console.log

---
+++ index.js
@@ -0,0 +1,41 @@
+// Prints the message
+console.log("Hello Worlds");
+// Variables
+
+// var (ES5)
+
+var broadwayInfosysTeacher = "raktim";
+broadwayInfosysTeacher = "shyam";
+// Always use camelCase for joining two or more words for variables
+console.log(broadwayInfosysTeacher);
+
+// let , const (ES6)
+
+let teacher = "raktim";
+teacher = "sita";
+console.log(teacher);
+
+const teach = "raktim";
+console.log(teach);
+
+// Writing comment (single line comment)
+/* Create 4 variables using const 
+keyword 
+for 
+
+table, 
+fan, 
+chair, 
+pen
+ (multi-line comment)
+*/
+
+const table = "red table";
+const blackFan = "khaitan";
+const woodenChair = "furniture";
+const redPen = "champion pen";
+
+console.log(table + blackFan, woodenChair, redPen);
+console.log(2 + 2);
+
+// use var and let keyword for 4 variables
